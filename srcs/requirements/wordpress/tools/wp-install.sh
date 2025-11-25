92b9ef1b1661e2eeebf64305fb1f5eee506e18f5
OmegaWiz <kraiwish123@gmail.com>
2025-11-25 14:12:02 +0700

    /

--- srcs/requirements/wordpress/tools/wp-install.sh
+++ srcs/requirements/wordpress/tools/wp-install.sh
@@ -1,5 +1,30 @@
 #!/bin/sh
 
+# Read Docker secrets if they exist, otherwise use environment variables
+if [ -f /run/secrets/mysql_root_password ]; then
+    MYSQL_ROOT_PASSWORD=$(cat ${MYSQL_ROOT_PASSWORD_FILE})
+fi
+
+if [ -f /run/secrets/mysql_password ]; then
+    MYSQL_PASSWORD=$(cat ${MYSQL_PASSWORD_FILE})
+fi
+
+if [ -f /run/secrets/wp_admin_password ]; then
+    WP_ADMIN_PASSWORD=$(cat ${WP_ADMIN_PASSWORD_FILE})
+fi
+
+if [ -f /run/secrets/wp_test_password ]; then
+    WP_TEST_PASSWORD=$(cat ${WP_TEST_PASSWORD_FILE})
+fi
+
+# Ensure required environment variables are set
+if [ -z "$MYSQL_ROOT_PASSWORD" ] || [ -z "$MYSQL_PASSWORD" ] || [ -z "$MYSQL_DATABASE" ] || [ -z "$MYSQL_USER" ] || [ -z "$WP_ADMIN_PASSWORD" ] || [ -z "$WP_TEST_PASSWORD" ]; then
+    echo "Error: Required environment variables are not set"
+    echo "MYSQL_ROOT_PASSWORD, MYSQL_PASSWORD, MYSQL_DATABASE, MYSQL_USER, WP_ADMIN_PASSWORD, and WP_TEST_PASSWORD must be defined"
+    exit 1
+fi
+
+
 # Wait for MariaDB to be ready
 # echo "Waiting for MariaDB to be ready..."
 # while ! mysqladmin ping -h"$MYSQL_HOSTNAME" --silent; do
